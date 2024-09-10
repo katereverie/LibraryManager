52f33b5c7c9e9485193e4c2d1bafc8d49c2bf22d
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-11 00:56:21 +0200

    add: tests

---
+++ LibraryManager.UnitTest/CheckoutServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagement.Test.MockRepos;
+using LibraryManager.Application.Services;
+using NUnit.Framework;
+
+namespace LibraryManagement.Test
+{
+    [TestFixture]
+    public class CheckoutServiceTests
+    {
+        [Test]
+        public void CheckBorrowerStatus()
+        {
+            var service = new CheckoutService(new MockCheckoutRepo(), new MockMediaRepo());
+
+            // Borrower with ID 1 has 3 unreturned checked out items
+            // Borrower with ID 2 has 1 checked out item
+            // Borrower with ID 3 has overdue item
+            var result1 = service.CheckBorrowStatus(1);
+            var result2 = service.CheckBorrowStatus(2);
+            var result3 = service.CheckBorrowStatus(3);
+
+            Assert.That(result1.Ok, Is.False);
+            Assert.That(result1.Message, Is.EqualTo("Borrower has more than 3 checked-out items."));
+            Assert.That(result2.Ok, Is.True);
+            Assert.That(result3.Ok, Is.False);
+            Assert.That(result3.Message, Is.EqualTo("Borrower has overdue item."));
+        }
+    }
+}
---
+++ LibraryManager.UnitTest/MockRepositories/MockCheckoutRepository.cs
@@ -0,0 +1,93 @@
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Interfaces;
+
+namespace LibraryManagement.Test.MockRepos
+{
+    public class MockCheckoutRepo : ICheckoutRepository
+    {
+        private List<CheckoutLog> _logs = new List<CheckoutLog>
+        {
+            new CheckoutLog
+            {
+                CheckoutLogID = 1,
+                BorrowerID = 1,
+                MediaID = 1,
+                CheckoutDate = DateTime.Today,
+                DueDate = DateTime.Today.AddDays(7),
+                ReturnDate = null
+            },
+            new CheckoutLog
+            {
+                CheckoutLogID = 2,
+                BorrowerID = 1,
+                MediaID = 2,
+                CheckoutDate = DateTime.Today,
+                DueDate = DateTime.Today.AddDays(7),
+                ReturnDate = null
+            },
+            new CheckoutLog
+            {
+                CheckoutLogID = 3,
+                BorrowerID = 1,
+                MediaID = 3,
+                CheckoutDate = DateTime.Today,
+                DueDate = DateTime.Today.AddDays(7),
+                ReturnDate = null
+            },
+            new CheckoutLog
+            {
+                CheckoutLogID = 4,
+                BorrowerID = 2,
+                MediaID = 4,
+                CheckoutDate = DateTime.Today,
+                DueDate = DateTime.Today.AddDays(7),
+                ReturnDate = null
+            },
+            new CheckoutLog
+            {
+                CheckoutLogID = 5,
+                BorrowerID = 3,
+                MediaID = 5,
+                CheckoutDate = new DateTime(2024, 1, 1),
+                DueDate = new DateTime(2024, 1, 1).AddDays(7),
+                ReturnDate = null
+            }
+
+        };
+
+        public List<CheckoutLog> GetCheckoutLogsByBorrowerID(int borrowerID)
+        {
+            return _logs.FindAll(cl => cl.BorrowerID == borrowerID);
+        }
+
+        public int Add(CheckoutLog newCheckoutLog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CheckoutLog> GetAllCheckedoutMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Media> GetAvailableMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Borrower? GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CheckoutLog> GetCheckedoutMediaByBorrowerID(int borrowerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int checkoutLogID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
