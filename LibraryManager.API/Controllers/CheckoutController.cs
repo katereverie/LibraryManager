56bb95534b45913e6e3c64e28d4b70756c72f361
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-23 22:46:00 +0200

    upload API project

---
+++ LibraryManager.API/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager.API.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryManager.API/Controllers/MediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager.API.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
