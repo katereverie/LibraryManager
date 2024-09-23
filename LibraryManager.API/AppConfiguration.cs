dfbe26081ad25f55915a2f23a84e5bfe61345f20
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-23 22:55:18 +0200

    add AppConfig to API project

---
+++ LibraryManager.API/AppConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Interfaces;
+
+namespace LibraryManagement.UI
+{
+    public class AppConfiguration : IAppConfiguration
+    {
+        private IConfiguration _configuration;
+
+        public AppConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+
+        public string GetConnectionString()
+        {
+            return _configuration["LibraryDb"] ?? "";
+        }
+
+        public DatabaseAccessMode GetDatabaseAccessMode()
+        {
+            switch (_configuration["DatabaseAccessMode"])
+            {
+                case "ORM":
+                    return DatabaseAccessMode.ORM;
+                case "SQL":
+                    return DatabaseAccessMode.DirectSQL;
+                default:
+                    throw new Exception("DatabaseMode configuration key not found!");
+            }
+        }
+    }
+}
