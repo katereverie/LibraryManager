6231dd549b989e720254ee265cca0caf4e471dc3
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-24 08:08:34 +0200

    add DI for serviceFactory, add scoped to services, add Swagger UI, HTTPs Redirect, Authorization, Map Controllers

--- LibraryManager.API/Program.cs
+++ LibraryManager.API/Program.cs
@@ -1,3 +1,5 @@
+using LibraryManagement.UI;
+using LibraryManager.Application;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,24 @@ builder.Services.AddSwaggerGen(c =>
     c.IncludeXmlComments(xmlPath);
 });
 
+// configure DI for service factory
+var config = new AppConfiguration();
+var serviceFactory = new ServiceFactory(config);
+
+builder.Services.AddScoped(_ => serviceFactory.CreateBorrowerService());
+builder.Services.AddScoped(_ => serviceFactory.CreateMediaService());
+builder.Services.AddScoped(_ => serviceFactory.CreateCheckoutService());
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
