80c463c8a1ddb2747abf4db3d28e1905dc6b46c0
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-24 08:27:24 +0200

    add controller methods, documentation, logging, attributes

--- LibraryManager.API/Controllers/BorrowerController.cs
+++ LibraryManager.API/Controllers/BorrowerController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManager.API.Controllers
 {
     public class BorrowerController : Controller
     {
-        public IActionResult Index()
+        private readonly IBorrowerService _borrowerService;
+        private readonly ILogger<BorrowerController> _logger;
+
+        public BorrowerController(IBorrowerService borrowerService, ILogger<BorrowerController> logger)
         {
-            return View();
+            _borrowerService = borrowerService;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// List all borrowers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Borrower>), StatusCodes.Status200OK)]
+        public IActionResult GetAllBorrowers()
+        {
+            var result = _borrowerService.GetAllBorrowers();
+
+            if (result.Ok)
+            {
+                _logger.LogInformation("All borrowers retrieved successfully");
+                return Ok(result.Data);
+            }
+            
+            _logger.LogError("Error retrieving all borrowers. {Message}", result.Message);
+            return StatusCode(500, result.Message);
+        }
+
+        /// <summary>
+        /// View a borrower
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [HttpGet("{email}")]
+        [ProducesResponseType(typeof(Borrower), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetBorrower(string email)
+        {
+            var result = _borrowerService.GetBorrower(email);
+
+            if (result.Ok)
+            {
+                _logger.LogInformation("Borrower retrieved successfully.");
+                return Ok(result.Data);
+            }
+
+            if (result.Message.Contains("No Borrower"))
+            {
+                _logger.LogWarning(result.Message, $"Borrower with email {email} not found");
+                return NotFound();
+            }
+
+            _logger.LogError(result.Message, "Error retrieving a borrower.");
+            return StatusCode(500, result.Message);
+        }
+
+        public IActionResult AddBorrower(AddBorrower borrowerToAdd)
+        {
+
         }
     }
 }
