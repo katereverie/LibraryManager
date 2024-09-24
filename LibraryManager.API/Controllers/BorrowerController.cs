97f8ac7d9e71eed6f48e6c20e28ce2cfbbf938db
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-24 19:30:30 +0200

    add BorrowerController methods: AddBorrower, EditBorrower

--- LibraryManager.API/Controllers/BorrowerController.cs
+++ LibraryManager.API/Controllers/BorrowerController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManager.Core.Entities;
+﻿using LibraryManager.API.Models;
+using LibraryManager.Core.Entities;
 using LibraryManager.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,7 @@ namespace LibraryManager.API.Controllers
         /// List all borrowers
         /// </summary>
         /// <returns></returns>
-        [HttpGet]
+        [HttpGet("")]
         [ProducesResponseType(typeof(List<Borrower>), StatusCodes.Status200OK)]
         public IActionResult GetAllBorrowers()
         {
@@ -27,12 +28,12 @@ namespace LibraryManager.API.Controllers
 
             if (result.Ok)
             {
-                _logger.LogInformation("All borrowers retrieved successfully");
+                _logger.LogInformation("Borrowers retrieved successfully.");
                 return Ok(result.Data);
             }
-            
-            _logger.LogError("Error retrieving all borrowers. {Message}", result.Message);
-            return StatusCode(500, result.Message);
+
+            _logger.LogError("Error retrieving borrowers. Error: {ErrorMessage}", result.Message);
+            return StatusCode(500, "An unexpected error occurred while processing your request. Please try again later.");
         }
 
         /// <summary>
@@ -47,25 +48,107 @@ namespace LibraryManager.API.Controllers
         {
             var result = _borrowerService.GetBorrower(email);
 
+            if (result.Message.Contains("Borrower with email"))
+            {
+                _logger.LogWarning("Borrower not found. Email: {BorrowerEmail}", email);
+                return NotFound();
+            }
+
             if (result.Ok)
             {
                 _logger.LogInformation("Borrower retrieved successfully.");
                 return Ok(result.Data);
             }
 
-            if (result.Message.Contains("No Borrower"))
-            {
-                _logger.LogWarning(result.Message, $"Borrower with email {email} not found");
-                return NotFound();
-            }
-
-            _logger.LogError(result.Message, "Error retrieving a borrower.");
-            return StatusCode(500, result.Message);
+            _logger.LogError("Error retrieving borrowe. Error: {ErrorMessage}", result.Message);
+            return StatusCode(500, "An unexpected error occurred while processing your request. Please try again later.");
         }
 
+        /// <summary>
+        /// Add a borrower
+        /// </summary>
+        /// <param name="borrowerToAdd"></param>
+        /// <returns></returns>
+        [HttpPost("")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult AddBorrower(AddBorrower borrowerToAdd)
         {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Invalid model state when adding borrower. {ModelState}", ModelState);
+                return BadRequest(ModelState);
+            }
 
+            var entity = new Borrower
+            {
+                FirstName = borrowerToAdd.FirstName,
+                LastName = borrowerToAdd.LastName,
+                Email = borrowerToAdd.Email,
+                Phone = borrowerToAdd.Phone
+            };
+
+            var result = _borrowerService.AddBorrower(entity);
+
+            if (result.Ok)
+            {
+                _logger.LogInformation("Borrower added successfully. {BorrowerEmail}", entity.Email);
+                return Created();
+            }
+
+            if (result.Message.Contains("Borrower with email"))
+            {
+                _logger.LogWarning("Attempt to add borrower with existing email. {Message}", entity.Email);
+                return Conflict();
+            }
+
+            _logger.LogError("Error adding borrower. {BorrowerEmail} Error: {ErrorMessage}", entity.Email, result.Message);
+            return StatusCode(500, "An unexpected error occurred while processing your request. Please try again later.");
+        }
+
+        /// <summary>
+        /// Edit a borrower
+        /// </summary>
+        /// <param name="borrowerId"></param>
+        /// <param name="borrowerToEdit"></param>
+        /// <returns></returns>
+        [HttpPut("{borrowerId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult EditBorrower(int borrowerId, EditBorrower borrowerToEdit)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Invalid model state when editing borrower. {ModelState}", ModelState);
+                return BadRequest(ModelState);
+            }
+
+            var entity = new Borrower
+            {
+                FirstName = borrowerToEdit.FirstName,
+                LastName = borrowerToEdit.LastName,
+                Email = borrowerToEdit.Email,
+                Phone = borrowerToEdit.Phone
+            };
+
+            var result = _borrowerService.UpdateBorrower(entity);
+
+            if (result.Ok)
+            {
+                _logger.LogInformation("Borrower successfully updated.");
+                return NoContent();
+            }
+
+            if (result.Message.Contains("Borrower with email"))
+            {
+                _logger.LogWarning("Attempted email edit conflicts with an existing email. {Message}", result.Message);
+                return Conflict();
+            }
+
+            _logger.LogError("Error adding borrower. {BorrowerEmail} Error: {ErrorMessage}", entity.Email, result.Message);
+            return StatusCode(500, "An unexpected error occurred while processing your request. Please try again later.");
         }
     }
 }
