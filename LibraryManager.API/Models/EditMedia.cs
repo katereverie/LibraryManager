9a1f07b75a2b12120186ebe7efaa83cf24bbc691
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-25 11:56:02 +0200

    add model: EditMedia

---
+++ LibraryManager.API/Models/EditMedia.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.API.Models
+{
+    public class EditMedia
+    {
+        public int MediaID { get; set; }
+        public int MediaTypeID { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title field musn't be empty.")]
+        public string Title { get; set; }
+        public bool IsArchived { get; set; }
+
+    }
+}
