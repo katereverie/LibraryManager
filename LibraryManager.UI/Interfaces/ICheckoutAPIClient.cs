a38e8920b8105101a94147306c169756b01cc66c
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-13 14:43:25 +0200

    chore: move interfaces folder into root

---
+++ LibraryManager.UI/Interfaces/ICheckoutAPIClient.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManager.UI.Models;
+
+namespace LibraryManager.UI.Interfaces;
+
+public interface ICheckoutAPIClient
+{
+    Task<List<Media>> GetAvailableMediaAsync();
+    Task<List<CheckoutLog>> GetCheckoutLogAsync();
+    Task CheckoutMediaAsync(int mediaId, string borrowerEmail);
+    Task ReturnMediaAsync(int checkoutLogId);
+}
