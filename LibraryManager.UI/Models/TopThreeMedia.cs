11c553c9eb9890629bfbc10dda3f0afa6a05e941
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-13 14:46:56 +0200

    fix: missing right curly bracket

--- LibraryManager.UI/Models/TopThreeMedia.cs
+++ LibraryManager.UI/Models/TopThreeMedia.cs
@@ -5,5 +5,5 @@ public class TopThreeMedia
     public int MediaID { get; set; }
     public string MediaTypeName { get; set; }
     public string Title { get; set; }
-    public int CheckoutCount { get; set;
+    public int CheckoutCount { get; set; }
 }
