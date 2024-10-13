abe7b9b7b4d14ca63c88aa1434d79052b4869636
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-13 14:46:36 +0200

    refactor: modify into ClientAppConfiguration

---
+++ LibraryManager.UI/ClientAppConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManager.UI.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace LibraryManagement.UI;
+
+public class ClientAppConfiguration : IClientAppConfiguration
+{
+    private IConfiguration _configuration;
+
+    public ClientAppConfiguration()
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json")
+            .AddUserSecrets<Program>()
+            .Build();
+    }
+
+    public string GetBaseUrl()
+    {
+        return _configuration["BaseUrl"] ?? throw new Exception("Base URL configuration key missing");
+    }
+}
