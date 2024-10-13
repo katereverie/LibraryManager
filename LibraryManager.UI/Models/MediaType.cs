43a31bdd965a12eaae3572cc563ce8ba7cc4406d
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-13 13:17:44 +0200

    feat: add models for client API

---
+++ LibraryManager.UI/Models/MediaType.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManager.UI.Models;
+
+public class MediaType
+{
+    public int MediaTypeID { get; set; }
+    public string MediaTypeName { get; set; }
+
+    public List<Media>? Medias { get; set; }
+}
---
+++ LibraryManager.UI/Models/TopThreeMedia.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManager.UI.Models;
+
+public class TopThreeMedia
+{
+    public int MediaID { get; set; }
+    public string MediaTypeName { get; set; }
+    public string Title { get; set; }
+    public int CheckoutCount { get; set;
+}
