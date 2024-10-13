2eb7166ae880f02d052bb7d567a679c78e0619d8
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-13 17:55:53 +0200

    refactor: simplify AddBorrowerAsync

--- LibraryManager.UI/Interfaces/IBorrowerAPIClient.cs
+++ LibraryManager.UI/Interfaces/IBorrowerAPIClient.cs
@@ -6,7 +6,7 @@ public interface IBorrowerAPIClient
 {
     Task<List<Borrower>> GetAllBorrowersAsync();
     Task<Borrower> GetBorrowerAsync(string email);
-    Task<Borrower> AddBorrowerAsync(AddBorrowerRequest borrower);
+    Task AddBorrowerAsync(AddBorrowerRequest borrower);
     Task EditBorrowerAsync(EditBorrowerRequest borrower);
     Task DeleteBorrowerAsync(int borrowerID);
 }
