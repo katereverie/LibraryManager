fb9d37f193fe449e95ae88af55cc8e21cd339987
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-16 21:36:02 +0200

    refactor: modify AddMediaAsync not to return void

--- LibraryManager.UI/Interfaces/IMediaAPIClient.cs
+++ LibraryManager.UI/Interfaces/IMediaAPIClient.cs
@@ -7,7 +7,7 @@ public interface IMediaAPIClient
     Task<List<MediaType>> GetMediaTypesAsync();
     Task<List<Media>> GetMediaByTypeAsync(int mediaTypeID);
     Task<List<TopThreeMedia>> GetMostPopularMediaAsync();
-    Task<Media> AddMediaAsync(AddMediaRequest media);
+    Task AddMediaAsync(AddMediaRequest media);
     Task EditMediaAsync(Media media);
     Task ArchiveMediaAsync(Media media);
     Task<List<Media>> GetArchivedMediaAsync();
