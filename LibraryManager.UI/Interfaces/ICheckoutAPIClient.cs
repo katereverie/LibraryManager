30689c50a2dee25d48589e1f196552d9c36cd65b
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-16 21:34:56 +0200

    refactor: rename for better readability

--- LibraryManager.UI/Interfaces/ICheckoutAPIClient.cs
+++ LibraryManager.UI/Interfaces/ICheckoutAPIClient.cs
@@ -5,7 +5,7 @@ namespace LibraryManager.UI.Interfaces;
 public interface ICheckoutAPIClient
 {
     Task<List<Media>> GetAvailableMediaAsync();
-    Task<List<CheckoutLog>> GetCheckoutLogAsync();
+    Task<List<CheckoutLog>> GetCurrentCheckoutLogsAsync();
     Task CheckoutMediaAsync(int mediaId, string borrowerEmail);
     Task ReturnMediaAsync(int checkoutLogId);
 }
