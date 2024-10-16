395a8d8524702a6ec413974c2b06c2adab266cd6
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-17 01:17:04 +0200

    feat: add ViewBorrowerDTO for ViewBorrower workflow

---
+++ LibraryManager.Core/Entities/ViewBorrowerDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryManager.Core.Entities
+{
+    public class ViewBorrowerDTO
+    {
+        public int BorrowerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<CheckoutLogDTO>? CheckoutLogs { get; set; }
+    }
+
+    public class CheckoutLogDTO
+    {
+        public DateTime CheckoutDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int MediaID { get; set; }
+        public string Title { get; set; }
+    }
+}
