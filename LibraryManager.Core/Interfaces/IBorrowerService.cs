645122cf41ad749936e12c39231d04978db7df5b
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-16 21:44:33 +0200

    refactor: modify method name for clarity

--- LibraryManager.Core/Interfaces/IBorrowerService.cs
+++ LibraryManager.Core/Interfaces/IBorrowerService.cs
@@ -6,7 +6,7 @@ public interface IBorrowerService
 {
     Result<List<Borrower>> GetAllBorrowers();
     Result<Borrower> GetBorrower(string email);
-    Result<List<CheckoutLog>> GetCheckoutLogsByBorrower(Borrower borrower);
+    Result<List<CheckoutLog>> GetCheckoutLogsByEmail(string email);
     Result UpdateBorrower(Borrower borrower);
     Result AddBorrower(Borrower newBorrower);
     Result DeleteBorrower(Borrower Borrower);
