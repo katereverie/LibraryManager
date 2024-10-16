4523e0f1ad6de7fbce05e9a4d93f9296c593d949
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-16 21:55:59 +0200

    refactor: too much to summarize

--- LibraryManager.UI/Workflows/CheckoutWorkflows.cs
+++ LibraryManager.UI/Workflows/CheckoutWorkflows.cs
@@ -1,110 +1,105 @@
 ﻿using LibraryManager.UI.Interfaces;
+using LibraryManager.UI.Utilities;
 
 namespace LibraryManager.UI.Workflows;
 
 public static class CheckoutWorkflows
 {
-    public static async Task Checkout(ICheckoutAPIClient client)
+    public static async Task CheckoutMedia(ICheckoutAPIClient client)
     {
         Console.Clear();
 
-        Borrower borrower;
-        string email = IO.GetRequiredString("Enter Borrower's Email: ");
-        var getBorrowerResult = client.GetBorrowerByEmail(email);
-
-        if (!getBorrowerResult.Ok)
+        try
         {
-            Console.WriteLine(getBorrowerResult.Message);
-            IO.AnyKey();
-            return;
-        }
-
-        borrower = getBorrowerResult.Data;
-
-        int exitChoice = 0;
-        while (exitChoice != 2)
-        {
-            var getMediaResult = client.GetAvailableMedia();
-
-            if (!getMediaResult.Ok)
+            var email = IO.GetRequiredString("Enter Borrower's Email: ");
+            int exitChoice = 0;
+            while (exitChoice != 2)
             {
-                Console.WriteLine(getMediaResult.Message);
-                IO.AnyKey();
-                return;
+                var availableMedias = await client.GetAvailableMediaAsync();
+                if (!availableMedias.Any())
+                {
+                    Console.WriteLine("No available media items found.");
+                    break;
+                }
+                Console.Clear();
+                IO.PrintAvailableMedia(availableMedias);
+                int mediaID = IO.GetMediaID(availableMedias, "Enter the ID of the media to check out: ");
+                await client.CheckoutMediaAsync(mediaID, email);
+                Console.WriteLine("Media successfully checked out.");
+                exitChoice = IO.GetCheckoutOption();
             }
-
-            var mediaList = getMediaResult.Data;
-            IO.PrintAvailableMedia(mediaList);
-
-            int mediaID = IO.GetMediaID(mediaList, "Enter the ID of the media to check out: ");
-
-            var checkoutResult = client.CheckoutMedia(mediaID, borrower.BorrowerID);
-
-            Console.WriteLine(checkoutResult.Ok ? "Media successfully checked out." : checkoutResult.Message);
-           
-            exitChoice = IO.GetCheckoutOption();
         }
-    }
-
-    public static async Task Return(ICheckoutAPIClient client)
-    {
-        Console.Clear();
-
-        Borrower borrower;
-        string email = IO.GetRequiredString("Enter Borrower's Email: ");
-        var getBorrowerResult = client.GetBorrowerByEmail(email);
-
-        if (!getBorrowerResult.Ok)
+        catch (Exception ex)
         {
-            Console.WriteLine(getBorrowerResult.Message);
-            IO.AnyKey();
-            return;
-        }
-
-        borrower = getBorrowerResult.Data;
-
-        int returnOption = 0;
-        while (returnOption != 2)
-        {
-            var getCheckoutLogResult = client.GetCheckedOutMediaByBorrowerID(borrower.BorrowerID);
-            if (!getCheckoutLogResult.Ok)
-            {
-                Console.WriteLine(getCheckoutLogResult.Message);
-                IO.AnyKey();
-                return;
-            }
-
-            var checkoutLogList = getCheckoutLogResult.Data;
-
-            IO.PrintBorrowersCheckedoutMedia(checkoutLogList);
-
-            int logID = IO.GetCheckoutLogID(checkoutLogList, "Enter the Log ID to return: ");
-            var returnResult = client.ReturnMedia(logID);
-
-            Console.WriteLine(returnResult.Ok ? "Return successfull" : returnResult.Message);
-
-            if (checkoutLogList.Count == 1)
-                break;
-
-            returnOption = IO.GetReturnOption();
+            Console.WriteLine($"API request failed.\n{ex.Message}");
         }
 
         IO.AnyKey();
     }
 
-    public static async Task CheckoutLog(ICheckoutAPIClient client)
+    public static async Task ReturnMedia(ICheckoutAPIClient client)
     {
         Console.Clear();
 
-        var getMediaResult = client.GetAllCheckedoutMedia();
+        try
+        {
+            var email = IO.GetRequiredString("Enter Borrower's Email: ");
 
-        if (!getMediaResult.Ok)
-        {
-            Console.WriteLine(getMediaResult.Message);
+            int returnOption = 0;
+            while (returnOption != 2)
+            {
+                var currentCheckoutLogs = await client.GetCurrentCheckoutLogsAsync();
+                var borrowerCheckoutLogs = currentCheckoutLogs.FindAll(cl => cl.Borrower.Email == email);
+
+                if (borrowerCheckoutLogs.Any())
+                {
+                    Console.Clear();
+                    IO.PrintBorrowersCheckedoutMedia(borrowerCheckoutLogs);
+                    int logID = IO.GetCheckoutLogID(borrowerCheckoutLogs, "Enter the Log ID to return: ");
+                    await client.ReturnMediaAsync(logID);
+                    Console.WriteLine($"Media successfully returned.");
+                    if (borrowerCheckoutLogs.Count == 1)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Either borrower with {email} is not registered or borrower has not checked out any media items");
+                    break;
+                }
+
+                returnOption = IO.GetReturnOption();
+            }
         }
-        else
+        catch (Exception ex)
         {
-            IO.PrintCheckoutLogList(getMediaResult.Data);
+            Console.WriteLine($"API request failed.\n{ex.Message}");
+        }
+
+        IO.AnyKey();
+    }
+
+    public static async Task GetCurrentCheckoutLogs(ICheckoutAPIClient client)
+    {
+        Console.Clear();
+
+        try
+        {
+            var currentCheckoutLogs = await client.GetCurrentCheckoutLogsAsync();
+
+            if (currentCheckoutLogs.Any())
+            {
+                IO.PrintCheckoutLogList(currentCheckoutLogs);
+            }
+            else
+            {
+                Console.WriteLine("No checkout logs found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"API request failed.\n{ex.Message}");
         }
 
         IO.AnyKey();
