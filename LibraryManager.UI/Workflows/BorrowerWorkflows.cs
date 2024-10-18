cfe8524aa88f238e6d456038ee92cee0d0f300fd
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-18 10:19:11 +0200

    refactor: move hint to GetEditedString

--- LibraryManager.UI/Workflows/BorrowerWorkflows.cs
+++ LibraryManager.UI/Workflows/BorrowerWorkflows.cs
@@ -93,7 +93,6 @@ public static class BorrowerWorkflows
             }
             else
             {
-                Console.WriteLine("Edit Borrower (Press \"Enter\" to skip)");
                 var editedBorrower = new EditBorrowerRequest
                 {
                     BorrowerID = borrower.BorrowerID,
