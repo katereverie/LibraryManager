5778a84a904d21c0f3cd4ad6b2fb56c85c00f610
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-24 00:38:15 +0200

    feat: add BorrowerForm
    - validate fields (make borrowerID nullable for getting user input
    - add overload constructor methods
    - add methods for converting entity to DTO, and DTO to entity

---
+++ LibraryManager.MVC/Models/BorrowerForm.cs
@@ -0,0 +1,54 @@
+﻿using LibraryManager.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.MVC.Models;
+
+public class BorrowerForm
+{
+    public int? BorrowerID { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Phone { get; set; }
+
+    /// <summary>
+    /// initialize default BorrowerForm DTO
+    /// </summary>
+    public BorrowerForm()
+    {
+        
+    }
+
+    /// <summary>
+    /// transport Borrower entity data to BorrowerForm fields
+    /// </summary>
+    /// <param name="entity"></param>
+    public BorrowerForm(Borrower entity)
+    {
+        BorrowerID = entity.BorrowerID;
+        FirstName = entity.FirstName;
+        LastName = entity.LastName;
+        Email = entity.Email;
+        Phone = entity.Phone;
+    }
+
+    /// <summary>
+    /// transport BorrowerForm DTO to Borrower entity
+    /// </summary>
+    /// <returns></returns>
+    public Borrower ToEntity()
+    {
+        return new Borrower
+        {
+            BorrowerID = BorrowerID ?? 0,
+            FirstName = FirstName,
+            LastName = LastName,
+            Email = Email,
+            Phone = Phone,
+        };
+    }
+}
