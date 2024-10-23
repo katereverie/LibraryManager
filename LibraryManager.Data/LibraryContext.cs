cdc6a932b7344b0dba490627b8b4a7f3be1665b0
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-23 08:52:11 +0200

    feat: configure entity relationships and contraints in context

--- LibraryManager.Data/LibraryContext.cs
+++ LibraryManager.Data/LibraryContext.cs
@@ -21,4 +21,46 @@ public class LibraryContext : DbContext
         optionsBuilder.UseSqlServer(_connectionString);
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Borrower>(e =>
+        {
+            e.HasKey(b => b.BorrowerID);
+            e.Property(b => b.FirstName).IsRequired();
+            e.Property(b => b.LastName).IsRequired();
+            e.Property(b => b.Email).IsRequired();
+            e.Property(b => b.Phone).IsRequired();
+            e.HasMany(b => b.CheckoutLogs)
+            .WithOne(c => c.Borrower)
+            .HasForeignKey(c => c.BorrowerID);
+        });
+
+        modelBuilder.Entity<Media>(e =>
+        {
+            e.HasKey(m => m.MediaID);
+            e.Property(m => m.Title).IsRequired();
+            e.HasOne(m => m.MediaType)
+            .WithMany(mt => mt.Medias)
+            .HasForeignKey(m => m.MediaTypeID)
+            .OnDelete(DeleteBehavior.Restrict);
+            e.HasMany(m => m.CheckoutLogs)
+            .WithOne(c => c.Media)
+            .HasForeignKey(c => c.MediaID)
+            .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<MediaType>(e =>
+        {
+            e.HasKey(mt => mt.MediaTypeID);
+            e.Property(mt => mt.MediaTypeName).IsRequired();
+        });
+
+        modelBuilder.Entity<CheckoutLog>(e =>
+        {
+            e.HasKey(c => c.CheckoutLogID);
+            e.Property(c => c.CheckoutDate).IsRequired();
+            e.Property(c => c.DueDate).IsRequired();
+        });
+    }
+
 }
