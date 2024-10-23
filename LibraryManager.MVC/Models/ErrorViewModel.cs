7c12b7cf3baecb686878b1f32d9ee125fba4c24e
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-23 23:54:32 +0200

    feat: init MVC project

---
+++ LibraryManager.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManager.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
