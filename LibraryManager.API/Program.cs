087f4c3855edb860dcf2329bfed94f53a8858d23
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-23 08:55:32 +0200

    feat: configure JSON serialization settings
    - prevent circular reference exception
    - enable indented formatting

--- LibraryManager.API/Program.cs
+++ LibraryManager.API/Program.cs
@@ -1,13 +1,20 @@
 using LibraryManagement.API;
 using LibraryManager.Application;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
