73f77bf5f47cb053b728ca9161f418b171d30d92
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-24 00:03:10 +0200

    feat: add AppConfiguration with implementations
    - implement method to get connection string
    - implement method to get database access mode

---
+++ LibraryManager.MVC/AppConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Interfaces;
+
+namespace LibraryManager.MVC;
+
+public class AppConfiguration : IAppConfiguration
+{
+    private IConfiguration _configuration;
+
+    public AppConfiguration()
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>()
+            .Build();
+    }
+
+    public string GetConnectionString()
+    {
+        return _configuration["LibraryDb"] ?? "";
+    }
+
+    public DatabaseAccessMode GetDatabaseAccessMode()
+    {
+        switch (_configuration["DatabaseAccessMode"])
+        {
+            case "ORM":
+                return DatabaseAccessMode.ORM;
+            case "SQL":
+                return DatabaseAccessMode.DirectSQL;
+            default:
+                throw new Exception("DatabaseMode configuration key not found!");
+        }
+    }
+}
