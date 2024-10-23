e0214f2c91d440e1e990847883d02f7ed969ed8f
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-24 01:36:24 +0200

    feat: add MediaTypeForm

---
+++ LibraryManager.MVC/Models/MediaTypeForm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.MVC.Models;
+
+public class MediaTypeForm
+{
+    public int MediaTypeID { get; set; }
+    [Required]
+    public string MediaTypeName { get; set; }
+}
