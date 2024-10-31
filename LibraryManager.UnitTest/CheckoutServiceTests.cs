836e65b2bc6ab404fe2612ce10277a87c1e5797f
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-10-31 18:10:19 +0100

    refactor: adapt tests due to repository interface changes

--- LibraryManager.UnitTest/CheckoutServiceTests.cs
+++ LibraryManager.UnitTest/CheckoutServiceTests.cs
@@ -2,18 +2,19 @@
 using LibraryManager.Application.Services;
 using NUnit.Framework;
 
-namespace LibraryManagement.Test;
+namespace LibraryManager.UnitTest;
 
 [TestFixture]
 public class CheckoutServiceTests
 {
     [Test]
-    public void CheckoutMedia_BorrowerHasMoreThanThreeCheckedoutItems()
+    public void CheckoutMedia_BorrowerHasMoreThanThreeCheckedOutItems()
     {
-        var service = new CheckoutService(new MockCheckoutRepo(), new MockMediaRepo());
-
+        var service = new CheckoutService(new MockCheckoutRepo(), new MockBorrowerRepo());
+        
+        
         // Borrower with ID 1 has 3 unreturned checked out items
-        var result = service.CheckoutMedia(6, 1);
+        var result = service.CheckoutMedia(6, "test@example.com");
 
         Console.WriteLine(result.Message);
         Assert.That(result.Ok, Is.False);
@@ -23,10 +24,10 @@ public class CheckoutServiceTests
     [Test]
     public void CheckoutMedia_BorrowerOverdueItems()
     {
-        var service = new CheckoutService(new MockCheckoutRepo(), new MockMediaRepo());
+        var service = new CheckoutService(new MockCheckoutRepo(), new MockBorrowerRepo());
 
         // Borrower with ID 3 has overdue item
-        var result = service.CheckoutMedia(6, 3);
+        var result = service.CheckoutMedia(6, "test@example.com");
 
         Console.WriteLine(result.Message);
 
@@ -37,10 +38,10 @@ public class CheckoutServiceTests
     [Test]
     public void CheckoutMedia_Success()
     {
-        var service = new CheckoutService(new MockCheckoutRepo(), new MockMediaRepo());
+        var service = new CheckoutService(new MockCheckoutRepo(), new MockBorrowerRepo());
 
         // Borrower with ID 2 has 1 checked out item
-        var result = service.CheckoutMedia(6, 2);
+        var result = service.CheckoutMedia(6, "test@example.com");
 
         Assert.That(result.Ok, Is.True);
     }
