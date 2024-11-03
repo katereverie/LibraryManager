856ea2f63065c78d62e858c122fb4b000ce07fc8
jiantao <143613315+Katereverie@users.noreply.github.com>
2024-11-03 21:10:13 +0100

    feat: add MediaModel

---
+++ LibraryManager.MVC/Models/MediaModel.cs
@@ -0,0 +1,32 @@
+﻿using LibraryManager.Core.Entities;
+
+namespace LibraryManager.MVC.Models;
+
+public class MediaModel
+{
+    public int? MediaID { get; set; }
+    public int? MediaTypeID { get; set; }
+    public string? Title { get; set; }
+    public bool IsArchived { get; set; }
+
+    public MediaModel () { }
+
+    public MediaModel (Media entity)
+    {
+        MediaID = entity.MediaID;
+        MediaTypeID = entity.MediaTypeID;
+        Title = entity.Title;
+        IsArchived = entity.IsArchived;
+    }
+
+    public Media ToEntity()
+    {
+        return new Media
+        {
+            MediaID = MediaID ?? 0,
+            MediaTypeID = MediaTypeID ?? 0,
+            Title = Title ?? string.Empty,
+            IsArchived = IsArchived
+        };
+    }
+}
