c1ba3289a8ee27915c828572a224c42fe2a83118
jiantao <143613315+Katereverie@users.noreply.github.com>
2024-11-03 21:12:10 +0100

    feat: add basic Search actions (get and put)

--- LibraryManager.MVC/Controllers/MediaController.cs
+++ LibraryManager.MVC/Controllers/MediaController.cs
@@ -1,5 +1,8 @@
-﻿using LibraryManager.Core.Interfaces;
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Interfaces;
+using LibraryManager.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LibraryManager.MVC.Controllers;
 
@@ -12,8 +15,51 @@ public class MediaController : Controller
         _mediaService = mediaService;
     }
 
-    public IActionResult Index()
+    [HttpGet]
+    public IActionResult Search()
     {
-        return View();
+        var result = _mediaService.GetAllMediaTypes();
+
+        if (!result.Ok)
+        {
+            TempData["ErrorMessage"] = result.Message;
+        }
+
+        var model = new MediaTypeForm
+        {
+            MediaTypes = new SelectList(result.Data, "MediaTypeID", "MediaTypeName")
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Search(List<MediaTypeModel> mediaTypes, int mediaTypeID, string? title, bool includeArchived = false)
+    {
+        var result = _mediaService.GetMediaByType(mediaTypeID);
+        List<Media> selectedMedia = new();
+
+        if (includeArchived)
+        {
+            // pitfall: assuming result.Data is not null  
+            selectedMedia = title == null 
+                ? result.Data 
+                : result.Data.FindAll(m => m.Title.Contains(title));
+        }
+        else
+        {
+            selectedMedia = title == null 
+                ? result.Data.FindAll(m => m.IsArchived == false) 
+                : result.Data.FindAll(m => m.IsArchived == false && m.Title.Contains(title));
+        }
+
+        var model = new MediaTypeForm
+        {
+            MediaTypes = new SelectList(mediaTypes, "MediaTypeID", "MediaTypeName"),
+            Title = title,
+            Medias = selectedMedia.Select(m => new MediaModel(m)).ToList(),
+        };
+
+        return View(model);
     }
 }
